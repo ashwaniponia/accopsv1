31dfbc5958a1018b1d5de33c4ac32d792c5a415d
unknown <karan8bajwa99@gmail.com>
2020-04-25 13:02:18 +0530

    Update user component almost finished

---
+++ src/app/updatetheuser.ts
@@ -0,0 +1,14 @@
+export interface Updatetheuser{
+    urights : string[],
+    drights : string[],
+    username : string,
+    imge : string ,
+    company : string,
+    address : string,
+    city : string,
+    country : string,
+    firstname : string,
+    lastname : string,
+    orgcode : string,
+    postalcode : number
+}
--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User1 } from "./User1";
+import { Updatetheuser } from "./updatetheuser";
 @Injectable({
   providedIn: 'root'
 })
@@ -9,22 +10,38 @@ export class UserServiceService {
   private _getUrl = "/api/viewusers";
   private _remApi = "/api/removeuser";
   private _addApi = "/api/adduser";
+  private _getUserUrl = "/api/updateuser/get";
+  private _post = "/api/updateuser/post";
 
   constructor(private _http:HttpClient
   ) { }
 
+
+  postUser(ans : Updatetheuser) : Observable<any>
+  {
+    return this._http.post(this._post , {'form' : ans});
+  }
 
   getUsers() : Observable<User1[]>
   {
     return this._http.get<User1[]>(this._getUrl);
   }
 
+  getUserinfo(username : string) : Observable<Updatetheuser>
+  {
+    const param1 = new HttpParams().set('username' , username);
+    const arr =  this._http.get<Updatetheuser>(this._getUserUrl , {params : param1});
+    console.log(arr);
+    return arr;
+  }
+
+
   removeUser(id): Observable<any> {
    return this._http.get(`${this._remApi}/delete/${id}`);
-  } 
+  }
 
   addUser(address,city,company,country,firstname,lastname,orgcode,postalcode,username): Observable<any>{
-    
+
     let img = "ABC",totaldeals =0,acceptedeals=0,rejecteddeals=0,dealspending=0,maxval=1000,Hide=true,Hide1=false
     const obj ={
       username,img,company,address,city,country,firstname,lastname,orgcode,postalcode,totaldeals,acceptedeals,rejecteddeals,dealspending,maxval,Hide,Hide1
